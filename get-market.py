8653c64b651cc84b4e944e4b9cde5f5db9157f0a
Daniel Ratcliffe <daniel.j.ratcliffe@gmail.com>
2015-12-18 09:54:44 -0800

    Basic message retrieval implemented

--- get-market.py
+++ get-market.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python
 
-print 'test'
+import zlib, zmq, simplejson
+
+def main():
+    context = zmq.Context()
+    subscriber = context.socket(zmq.SUB)
+    
+    subscriber.connect('tcp://relay-us-west-1.eve-emdr.com:8050')
+    
+    subscriber.setsockopt(zmq.SUBSCRIBE, "")
+    
+    while True:
+        market_json = zlib.decompress(subscriber.recv())
+        
+        market_data = simplejson.loads(market_json)
+        
+        print market_data
+        
+if __name__ == '__main__':
+    main()
